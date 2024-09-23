4cb43b25e014201d0e1941710e9555adac48ad42
Mudz <mudz.majid@gmail.com>
2024-09-23 14:47:05 +0800

    fix link in stock update history

--- resources/views/histories/show.blade.php
+++ resources/views/histories/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-            <span class="font-semibold text-md text-gray-800 leading-tight">{{ __('Main') }}</span>
+            <a class="font-semibold text-md text-gray-800 leading-tight" href="{{route('home')}}">{{ __('Main') }}</a> \ Stock Update History
     </x-slot>
     <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
